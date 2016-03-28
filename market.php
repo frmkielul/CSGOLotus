4ff7036fd3cab7033b2c39d569ecc0c4058712e2
Frank Paschen <frankpaschen99@gmail.com>
2016-03-28 15:30:56 -0400

    Added Inspect in Game. Sent assetid in POST instead of market_hash_name

--- market.php
+++ market.php
@@ -1,25 +1,36 @@
 <?php
 require_once("config.php");
 
-if (!logged_in()) {
+/*if (!logged_in()) {
 	echo "Not logged in. Click " . "<a href=index.php>here</a>" . " to return to the homepage.";
-	die();	// prevent any unnecessary calls to price_check and price_check_credits which slow down the page a lot.
-}
+	//die();	// prevent any unnecessary calls to price_check and price_check_credits which slow down the page a lot.
+}*/
 
 $json_obj = json_decode(file_get_contents("scruffybot_inv.txt"), true);
-$items = $json_obj['rgDescriptions'];
+$descriptions = $json_obj['rgDescriptions'];
+$inventory = $json_obj['rgInventory'];
+file_put_contents("scruffybot_inv.txt", file_get_contents("http://steamcommunity.com/id/thescruffybot/inventory/json/730/2"));
 
 echo "<h2>CSGOLotus Market</h2>";
 echo "&#8353; = CSGOLotus Credits<br/>";
 echo "$ = U.S. Dollars";
 
+// TODO: to allow cases to be displayed, we must find a common identifier for cases, and exclude them from the $a['actions'][0]['link'] call.
 echo "<form action=purchase.php method=POST>";
-foreach ($items as $a) {
+foreach ($descriptions as $a) {
 	$img_url = "http://cdn.steamcommunity.com/economy/image/" . $a['icon_url'];
-	echo "<input type='checkbox' name='item[]' value=\"" . $a['market_hash_name'] . "\"><img src=".$img_url." height='64' width='64'/>" . $a['market_hash_name'] . " " . price_check_credits(array($a['market_hash_name'])) . " | " . price_check(array($a['market_hash_name'])) . "<br>";
-	
-	//echo "<input type='checkbox' name='item' value=\"" . $a['market_hash_name'] . "\"><br />";
+	$value = "";
+	foreach ($inventory as $x) {
+		if ($a['classid'] == $x['classid'] && $a['instanceid'] == $x['instanceid']) {
+			$value = $x['id'];
+		}
+	}
+	$inspect_url = $a['actions'][0]['link'];
+	$url_fragment = explode("%", $inspect_url)[5];
+	echo "<input type='checkbox' name='item[]' value=\"" . $a['market_hash_name'] . "/" . $value . "\"><img src=".$img_url." height='64' width='64'/>" . $a['market_hash_name'] . " " . 
+	price_check_credits(array($a['market_hash_name'])) . " | " . price_check(array($a['market_hash_name'])) . " | <a href=steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20S76561198180102897A" . $value . $url_fragment .">Inspect In-Game</a><br>";
 }
+
 echo "<input type=submit value=Purchase Items>";
 echo "</form>";
 ?>
--- purchase.php
+++ purchase.php
@@ -6,18 +6,24 @@ if (!logged_in()) {
 	die();	// we dont want people sending scruffybot incomplete data (steamid64 missing)
 }
 
-$selected_items = $_POST["item"];
+$market_hash_names = array();
+$asset_ids = array();
+foreach ($_POST["item"] as $x) {
+	array_push($market_hash_names, explode("/", $x)[0]);
+	array_push($asset_ids, explode("/", $x)[1]);
+}
 echo "<b>You have selected the following items: </b><br />";
 echo "<ul>";
-foreach ($selected_items as $a) {
+
+foreach ($market_hash_names as $a) {
 	echo "<li>" . $a . "</li><br />";
 }
 echo "</ul>";
 
 echo "Your current account balance: " . credits(get_steamid64(), $db) . "<br />";
-echo "Your account will be deducted " . price_check_credits($selected_items) . " credits for this transaction.";
+echo "Your account will be deducted " . price_check_credits($market_hash_names) . " credits for this transaction.";
 
 echo "<br /><br />";
-emit_data(create_network_data($selected_items, $db), $client);	
+emit_data(create_network_data($asset_ids, $db), $client);	
 echo "Your request has been processed and you will receieve a trade offer on Steam.<br />";
 ?>
