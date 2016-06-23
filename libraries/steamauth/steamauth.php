3146c5481eb4cede0d137eff5531ccb546f0bda3
Pedro Safi <jpsafi@gmail.com>
2016-06-23 18:58:27 -0400

    added login button

--- libraries/steamauth/steamauth.php
+++ libraries/steamauth/steamauth.php
@@ -14,20 +14,20 @@ function steamlogin($db)
     try {
         require("settings.php");
         $openid = new LightOpenID($steamauth['domainname']);
-        
+
         $button['small']    = "small";
         $button['large_no'] = "large_noborder";
         $button['large']    = "large_border";
         $button             = $button[$steamauth['buttonstyle']];
-        
+
         if (!$openid->mode) {
             if (isset($_GET['login'])) {
                 $openid->identity = 'http://steamcommunity.com/openid';
                 header('Location: ' . $openid->authUrl());
             }
-            echo "<form action=\"?login\" method=\"post\"> <input type=\"image\" src=\"http://cdn.steamcommunity.com/public/images/signinthroughsteam/sits_" . $button . ".png\"></form>";
+            echo "<a href='/?login' class='steambutton'><span>Login with Steam</span><div class='icon'><i class='fa fa-steam-square'></i></div></a>";
         }
-        
+
         elseif ($openid->mode == 'cancel') {
             echo 'User has canceled authentication!';
         } else {
@@ -35,19 +35,19 @@ function steamlogin($db)
                 $id  = $openid->identity;
                 $ptn = "/^http:\/\/steamcommunity\.com\/openid\/id\/(7[0-9]{15,25}+)$/";
                 preg_match($ptn, $id, $matches);
-                
+
                 $_SESSION['steamid'] = $matches[1];
-				
+
 				// user has successfully logged in from steam
 				register_firsttime($db, $matches[1]);
-				
+
                 if (isset($steamauth['loginpage'])) {
                     header('Location: ' . $steamauth['loginpage']);
                 }
             } else {
                 echo "User is not logged in.\n";
             }
-            
+
         }
     }
     catch (ErrorException $e) {
