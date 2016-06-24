ec646a6ed062d859c0959f3ce0fd1e31661792b2
Pedro Safi <jpsafi@gmail.com>
2016-06-23 22:50:13 -0400

    disabled zoom maybe

--- paths.php
+++ paths.php
@@ -11,7 +11,7 @@ require_once("config.php");
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
     <link href="css/paths_css.css" rel="stylesheet">
-    <meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <meta name="viewport" content="height=device-height, width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no, target-densitydpi=device-dpi" />
   </head>
   <body>
     <div id="wrapper">
