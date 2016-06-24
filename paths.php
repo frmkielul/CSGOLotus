61ca99af36e5888704ea8cb3ee7023cc0a9b86a6
Frank Paschen <frankpaschen99@gmail.com>
2016-06-24 00:40:30 -0400

    number formatting

--- paths.php
+++ paths.php
@@ -48,7 +48,7 @@ require_once("config.php");
                   </div>
                 </div>
               </div>';
-              echo '<div id="userinfo"><img id="balance" src="img/token.png">'.credits($_SESSION["steamid"], $db).'<img id= "avatar" src="'.$steamprofile["avatarfull"].'">Welcome, <span>' . $steamprofile['personaname'] . '</span><div id="icon"><a href="#"><i class="fa fa-cog"></i></a></div></div>';
+              echo '<div id="userinfo"><img id="balance" src="img/token.png">'.number_format(credits($_SESSION["steamid"], $db)).'<img id= "avatar" src="'.$steamprofile["avatarfull"].'">Welcome, <span>' . $steamprofile['personaname'] . '</span><div id="icon"><a href="#"><i class="fa fa-cog"></i></a></div></div>';
           }
       ?>
     </header>
