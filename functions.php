70aa0c01339f19778e793797c160e0e125af4b98
Pedro Safi <jpsafi@gmail.com>
2016-06-25 17:32:27 -0400

    stylistic changes

--- functions.php
+++ functions.php
@@ -176,9 +176,9 @@ function draw_market() {
 		$lotus_value = price_check_credits(array($a['market_hash_name']));
 		$inspect_url = build_inspect_link($a, $value);
 
-		echo $usd_value;
-		echo $lotus_value;
 		echo "<img src=".$img_url." height='128'/>";
+		echo $usd_value;
+		echo "(".$lotus_value.")";
 		echo "<a href=".$inspect_url.">Inspect in Game</a>";
 	}
 
