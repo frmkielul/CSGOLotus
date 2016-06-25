4d8a2b5894a012a862bfece49fb7298823a4601c
Pedro Safi <jpsafi@gmail.com>
2016-06-25 16:37:49 -0400

    FUCK THIS SHIT

--- index.php
+++ index.php
@@ -30,8 +30,8 @@ require_once("config.php");
         <li><a href="#">Game</a></li>
         <li><a href="#">Game</a></li>
         <li><a href="market.php">Market</a></li>
-        <li style="float:right;"><a href="account.php" style="background:#00638E;">Account</a></li>
-        <li style="float:right;"><a href="account.php" style="background:#00638E; border-right: 1px #005275">Deposit</a></li>
+        <li style="float:right;"><a href="account.php">Account</a></li>
+        <li style="float:right;"><a href="account.php" style="background:#00638E;">Deposit</a></li>
       </ul>
     </nav>
     </div>
