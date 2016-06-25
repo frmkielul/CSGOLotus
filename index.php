35a65a9e264ad794a0f77ec6cce7e70f2a963772
Pedro Safi <jpsafi@gmail.com>
2016-06-25 16:30:35 -0400

    123

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ require_once("config.php");
         <li><a href="#">Game</a></li>
         <li><a href="#">Game</a></li>
         <li><a href="market.php">Market</a></li>
-        <li style="float:right;"><a href="account.php" style="background:#00638E; font-weight: 800">Account</a></li>
+        <li style="float:right;"><a href="account.php" style="background:#00638E; font-weight: 600">Account</a></li>
       </ul>
     </nav>
     </div>
