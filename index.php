da9d304a0d0f13083dc24a3ef215afae6f20a392
Pedro Safi <jpsafi@gmail.com>
2016-06-25 16:43:21 -0400

    ay

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ require_once("config.php");
     <meta content="" name="description">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-    <link href="css/paths_css.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
     <meta name="viewport" content="height=device-height, width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no, target-densitydpi=device-dpi" />
   </head>
   <body>
