a0a616e7946c60d03cf6aeac0e1f4fc4f1d89754
Pedro Safi <jpsafi@gmail.com>
2016-06-26 15:14:52 -0400

    123

--- functions.php
+++ functions.php
@@ -176,8 +176,8 @@ function draw_market() {
 		$lotus_value = price_check_credits(array($a['market_hash_name']));
 		$inspect_url = build_inspect_link($a, $value);
 
-		echo "<img src=".$img_url." height='100'/>";
 		echo $lotus_value;
+		echo "<img src=".$img_url." height='100'/>";
 		echo "<a href=".$inspect_url.">Inspect in Game</a>";
 	}
 
